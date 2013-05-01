c236a075d650e3f58cc006a5864ffd69cc0cde40
CivilizedGravy <whatsthenumberto911@gmail.com>
2013-05-01 00:52:36 -0400

    init commit

---
+++ pi calc/src/picalc.java
@@ -0,0 +1,39 @@
+import java.math.BigDecimal;
+
+public class picalc {
+
+	public picalc() {
+		int k = 0;
+		double pip = 0;
+		// BBP Summation formula for PI
+		BigDecimal bd = new BigDecimal(pip);
+		
+		while (k!=100) {
+		
+			double denom = 8 * k;
+			double pow = 1 / (Math.pow(16, k));
+			double f = 4 / (denom + 1);
+			double s = 2 / (denom + 4);
+			double t = 1 / (denom + 5);
+			double fo = 1 / (denom + 6);
+			
+			BigDecimal pi = new BigDecimal(pow * (f - s - t - fo));
+			
+			
+			bd = bd.add(pi);
+			
+			k++;
+
+		}
+		System.out.println(bd.toString());
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new picalc();
+	}
+
+}
